a2f246f09a6208b49dd25aa401e2ad639e1f747a
Theuz <82974688+TheuZCoder@users.noreply.github.com>
2023-03-01 14:45:18 -0300

    Delete Java directory

--- Java/CalculadoraDeDesconto/App.java
+++
@@ -1,8 +0,0 @@
-package CalculadoraDeDesconto;
-
-public class App {
-    public static void main(String[] args) {
-        CalcDesconto cDesconto = new CalcDesconto();
-        cDesconto.calcular();
-    }
-}
